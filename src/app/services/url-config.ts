8d05ca4a01ff16b534deed95d8f3b2c339985c99
Manh Huy <1451010071Huy@ou.edu.vn>
2018-03-14 20:59:08 +0700

    fix loi

--- src/app/services/url-config.ts
+++ src/app/services/url-config.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-  public static API_ENDPOINT = '';//upload heroku
-  //ngpublic static API_ENDPOINT = 'http://localhost:3000';// local
+  //public static API_ENDPOINT = '';//upload heroku
+  public static API_ENDPOINT = 'http://localhost:3000';// local
 }
